3f8e4d7a1dbc55016020f6308ee6fdc3d550d2ca
sod-diq <mmsardiqque@gmail.com>
2023-11-21 15:51:44 +0100

    hooks was added

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,20 @@
+import { useState } from "react";
+
 const Home = () => {
+  // let name = 'habu';
+  const [name, setName] = useState('habu'); 
+  const[age, setAge] = useState(25)
 
   const handleClick = () => {
-    console.log('hell, ustaz🤞');
+   setName ('luigi');
+   setAge (30);
+   console.log('😘');
   }
   
   return ( 
     <div className="home">
       <h2>Homepage</h2>
+      <p>{ name } is { age } years</p>
       <button onClick={handleClick}>Click me</button>
     </div>
    );
